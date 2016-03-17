4144ad6eb4eca1be1b0ec093b44f14313adbb971
hiro <hiro0218@gmail.com>
2016-03-17 18:28:17 +0900

    タブ入力
    ・テキストエリア内のタブ入力を実装
    ・軽微な修正

--- works/editor/js/script.js
+++ works/editor/js/script.js
@@ -43,4 +43,24 @@ document.addEventListener("DOMContentLoaded", function(){
         window.clearInterval(timer);
     }, false);
 
+    // タブ入力を有効にする
+    enableInsertTAB(editor);
+
 }, false);
+
+
+function enableInsertTAB(element) {
+    element.onkeydown = function(e) {
+        if (e.keyCode === 9) {
+            var val = this.value;
+            var start = this.selectionStart;
+            var end = this.selectionEnd;
+
+            this.value = val.substring(0, start) + '\t' + val.substring(end);
+
+            this.selectionStart = this.selectionEnd = start + 1;
+
+            return false;
+        }
+    };
+}
